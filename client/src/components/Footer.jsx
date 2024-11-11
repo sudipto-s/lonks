56ba9da3984d46b1ad579a153a50dd668935db80
Sudipta Singha <singhaashoka03@gmail.com>
2024-11-11 17:11:35 +0530

    Update dashboard icons

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,11 +3,16 @@ import '../css/HeaderFooter.css'
 const Footer = () => {
    return (
       <footer className="footer">
-         <p>&copy; {new Date().getFullYear()} <a href="https://github.com/sudipto-s/lonks">Lonks</a>. All rights reserved.</p>
+         <p className="s-lonk">
+            &copy; {new Date().getFullYear()} &nbsp;
+            <a href="/s-lonk">Lonks</a>. All rights reserved.
+            @Sudipto
+         </p>
          <div className="social-media">
-            <a href="/s-lonk" target="_blank" rel="noopener noreferrer">GitHub</a> |
-            <a href="https://instagram.com/sudipto_.s" target="_blank" rel="noopener noreferrer">Instagram</a> |
-            <a href="https://linkedin.com/in/sudipto-singha" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+            <a href="//github.com/sudipto-s" target="_blank" rel="noopener noreferrer">GitHub</a> |
+            <a href="//instagram.com/sudipto_.s" target="_blank" rel="noopener noreferrer">Instagram</a> |
+            <a href="//linkedin.com/in/sudipto-singha" target="_blank" rel="noopener noreferrer">LinkedIn</a> |
+            <a href="//iconoir.com" target="_blank" rel="noopener noreferrer">Iconoir</a>
          </div>
       </footer>
    );
