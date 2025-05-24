87645ba7f5830cbb6325a6f1c4d3668747e1ac1c
Sudipta Singha <115441575+sudipto-s@users.noreply.github.com>
2025-05-24 09:35:03 +0530

    Change cron period to 10 mins

--- server/utils/cron.js
+++ server/utils/cron.js
@@ -5,7 +5,7 @@ const PING_URL = "https://lonks.onrender.com/"
 
 // Schedule a task to send a request every 13 minutes
 if(process.env.NODE_ENV === "production") {
-   cron.schedule("*/13 * * * *", async () => {
+   cron.schedule("*/10 * * * *", async () => {
       try {
          const response = await axios.get(PING_URL)
          console.log("Server Pinged:", response.status)
